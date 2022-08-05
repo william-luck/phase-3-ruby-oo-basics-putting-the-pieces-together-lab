5a3ed9a10a066e717c9b479e37c01993b21650a6
William Luck <luck.william.h@gmail.com>
2022-08-05 18:58:19 -0500

    commit tree 0c9debc4210a561f6ba579a0d364c4dbc88b05f6

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
 
+class Book
+
+    attr_accessor :author, :title, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
+
+
+
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
 # Make your shoe class here!
+require 'pry'
+
+
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts 'Your shoe is as good as new!'
+        @condition = 'new'
+    end
+
+end
+
+binding.pry
+0
+
+
